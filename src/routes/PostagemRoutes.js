e4a51d00dd85ac1dfef5448aab09408e9b73c722
MCarlosEduardoS <106968731+MCarlosEduardoS@users.noreply.github.com>
2022-12-01 10:48:53 -0300

    Delete src directory

--- src/routes/PostagemRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const Router = express.Router();
-
-const Post = require('../controllers/PostagemController.js');
-
-Router.get('/forum/posts', Post.listarAll);
-Router.get('/forum/posts/:postagem_id', Post.listarID);
-
-module.exports = Router;
